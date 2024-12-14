a3008978cbb2a5b233e2a8f0fc82af1d59e52e1e
Naveen <imnaveenbisht@gmail.com>
2024-12-14 17:29:24 +0530

    Add Moto Dolby atmos

--- device.mk
+++ device.mk
@@ -7,6 +7,9 @@
 # Call proprietary blob setup
 $(call inherit-product, vendor/realme/RM6785-common/RM6785-common-vendor.mk)
 
+# Make Dolby Atmos
+$(call inherit-product, hardware/dolby/dolby.mk)
+
 # Shipping API
 PRODUCT_SHIPPING_API_LEVEL := 29
 
