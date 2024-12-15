09eec1316d4a5744105f5c3aba2901f0a059c470
Naveen <imnaveenbisht@gmail.com>
2024-12-15 17:01:14 +0530

    VoltageOS bringup

---
+++ voltage_RM6785.mk
@@ -0,0 +1,27 @@
+#
+# Copyright (C) 2021-2022 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/non_ab_device.mk)
+
+# Inherit from device makefile
+$(call inherit-product, device/realme/RM6785/device.mk)
+
+# Inherit some common Voltage stuff.
+$(call inherit-product, vendor/voltage/config/common_full_phone.mk)
+
+# Device identifier. This must come after all inclusions.
+PRODUCT_NAME := voltage_RM6785
+PRODUCT_DEVICE := RM6785
+PRODUCT_BRAND := realme
+PRODUCT_MODEL := RM6785
+PRODUCT_MANUFACTURER := realme
+PRODUCT_GMS_CLIENTID_BASE := android-oppo
+
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay-voltage
