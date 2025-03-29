9750f00f5d0085b336918c7adf6ac80e67dfbc7d
Naveen <imnaveenbisht@gmail.com>
2025-03-29 11:28:50 +0530

    RM6785: AxionAOSP bringup
    Signed-off-by: Naveen <imnaveenbisht@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/clover_RM6785.mk
+    $(LOCAL_DIR)/lineage_RM6785.mk
 
 COMMON_LUNCH_CHOICES := \
-    clover_RM6785-user \
-    clover_RM6785-userdebug \
-    clover_RM6785-eng
+    lineage_RM6785-user \
+    lineage_RM6785-userdebug \
+    lineage_RM6785-eng
--- lineage_RM6785.mk
+++ lineage_RM6785.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2021-2022 The LineageOS Project
+# Copyright (C) 2021-2025 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -15,6 +15,18 @@ $(call inherit-product, device/realme/RM6785/device.mk)
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+
+# Axion Stuff
+AXION_CAMERA_REAR_INFO := 64,8,2,2
+AXION_CAMERA_FRONT_INFO := 16
+AXION_MAINTAINER := 𝙴𝙻𝙾𝙷𝙸𝙼
+AXION_PROCESSOR := MT6785
+TARGET_INCLUDE_VIPERFX := true
+
+# Axion CPU Flags
+AXION_CPU_SMALL_CORES := 0,1,2,3,4,5
+AXION_CPU_BIG_CORES := 6,7
+
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := lineage_RM6785
 PRODUCT_DEVICE := RM6785
@@ -22,3 +34,11 @@ PRODUCT_BRAND := realme
 PRODUCT_MODEL := RM6785
 PRODUCT_MANUFACTURER := realme
 PRODUCT_GMS_CLIENTID_BASE := android-oppo
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BuildDesc=$(call normalize-path-list, "sys_oplus_mssi_64_cn-user 11 RP1A.200720.011 1623809323039 release-keys")
+
+    BUILD_FINGERPRINT := realme/RMX2002/RMX2002L1:11/RP1A.200720.011/1651754371157:user/release-keys
+
+PRODUCT_PRODUCT_PROPERTIES += \
+   ro.build.fingerprint=$(BUILD_FINGERPRINT)
