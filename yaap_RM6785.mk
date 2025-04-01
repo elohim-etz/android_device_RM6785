2ec100f72b52af366b84872a3cb6535fbb107d57
Naveen <imnaveenbisht@gmail.com>
2025-04-01 22:22:20 +0530

    RM6785: Yaap bringup
    Signed-off-by: Naveen <imnaveenbisht@gmail.com>

--- yaap_RM6785.mk
+++ yaap_RM6785.mk
@@ -12,19 +12,15 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/non_ab_device.mk)
 # Inherit from device makefile
 $(call inherit-product, device/realme/RM6785/device.mk)
 
-# Inherit some common Clover stuff.
-$(call inherit-product, vendor/clover/config/common_full_phone.mk)
+# Inherit some common YAAP stuff.
+$(call inherit-product, vendor/yaap/config/common_full_phone.mk)
 
-# Project Clover Flags
-CLOVER_BUILDTYPE := UNOFFICIAL
-WITH_GMS := false
-TARGET_BUILD_VARIANT := user
-TARGET_FACE_UNLOCK_SUPPORTED := true
-
-# Boot animation
+# YAAP Flags
 TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_BUILD_GAPPS=false
+TARGET_ENABLE_BLUR=false
 
-PRODUCT_NAME := clover_RM6785
+PRODUCT_NAME := yaap_RM6785
 PRODUCT_DEVICE := RM6785
 PRODUCT_BRAND := realme
 PRODUCT_MODEL := RM6785
@@ -32,6 +28,9 @@ PRODUCT_MANUFACTURER := realme
 PRODUCT_GMS_CLIENTID_BASE := android-oppo
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildDesc="sys_oplus_mssi_64_cn-user 11 RP1A.200720.011 1623809323039 release-keys" \
-    BuildFingerprint=realme/RMX2002/RMX2002L1:11/RP1A.200720.011/1651754371157:user/release-keys \
-    DeviceProduct=RM6785 \
+    BuildDesc=$(call normalize-path-list, "sys_oplus_mssi_64_cn-user 11 RP1A.200720.011 1623809323039 release-keys")
+
+    BUILD_FINGERPRINT := realme/RMX2002/RMX2002L1:11/RP1A.200720.011/1651754371157:user/release-keys
+
+PRODUCT_PRODUCT_PROPERTIES += \
+   ro.build.fingerprint=$(BUILD_FINGERPRINT)
